bda450bb2acc5a1491e304268a72ac4c4c9c32aa
mrima <mrimamss@gmail.com>
2023-01-20 14:14:06 +0300

    map modal

---
+++ src/MapModal.jsx
@@ -0,0 +1,52 @@
+import { motion, AnimatePresence } from "framer-motion";
+import {useState} from "react";
+import styles from "./styles.js";
+import Map from "./Map.jsx";
+import "./App.css"
+
+
+export default function Modal() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const modalVariants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+    };
+
+    const backdropVariants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+    };
+    return (
+        <>
+            <button onClick={() => setIsModalOpen(true)}>Open Map</button>
+
+            <AnimatePresence>
+                {isModalOpen && (
+                    <>
+                        <motion.div
+                            variants={backdropVariants}
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                            style={styles.backdrop}
+                            onClick={() => setIsModalOpen(false)}
+                        />
+                        <motion.div
+                            variants={modalVariants}
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                            style={styles.modal}
+                        >
+                            <div className={"map-cont"}>
+                                <Map/>
+                            </div>
+                            <button onClick={() => setIsModalOpen(false)}>Close Map</button>
+                        </motion.div>
+                    </>
+                )}
+            </AnimatePresence>
+        </>
+    );
+}
